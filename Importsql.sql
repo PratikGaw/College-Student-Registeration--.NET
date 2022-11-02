5ab32b2e71a39c6266aff0ceaee6f58451dcb9c5
Pratik  Gawande <87464745+PratikGaw@users.noreply.github.com>
2022-11-02 21:20:49 +0530

    Create Importsql.sql

---
+++ Importsql.sql
@@ -0,0 +1,8 @@
+SqlConnection con = new SqlConnection("Data Source=KOBINATH-PC; Initial Catalog=gcbt; User Id=sa;
+Password=admin1234");
+SqlCommand cmd;
+SqlDataReader read;
+SqlDataAdapter drr;
+string id;
+bool Mode = true;
+string sql;
